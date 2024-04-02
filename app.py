c1e6883b0829611d7f84a8120e6059bbc33b4938
Dzithendo31 <mathegu.dzithendo@gmail.com>
2024-04-03 00:23:44 +0200

    Done some good front end on the home page, not much functionalisty byt year

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ app.register_blueprint(users_list_bp, url_prefix = "/usersList")
 app.register_blueprint(policy_bp, url_prefix = "/policies") 
 app.register_blueprint(adminBP, url_prefix = "/admin") 
 @app.route("/")
-def hello_world():
-    return "<p>Hddsddqq, 🥹</p>"
+def home():
+    return render_template("index.html")
 
 
 @app.get('/users/<id>')
@@ -44,57 +44,3 @@ def get_user(id):
         error = {'message':'User Not found'}
         return jsonify(error),404
     return jsonify(FoundUser),200
-
-
-# users = [
-#   {
-#       "id":12,
-#     "balance": "$3,946.45",
-#     "picture": "http://placehold.it/32x32",
-#     "age": 23,
-#     "name": "Bird Ramsey",
-#     "gender": "male",
-#     "company": "NIMON",
-#     "email": "birdramsey@nimon.com"
-#   },
-#   {
-#       "id":13,
-#     "balance": "$2,499.49",
-#     "picture": "http://placehold.it/32x32",
-#     "age": 31,
-#     "name": "Lillian Burgess",
-#     "gender": "female",
-#     "company": "LUXURIA",
-#     "email": "lillianburgess@luxuria.com"
-#   },
-#   {
-#       "id":14,
-#     "balance": "$2,820.18",
-#     "picture": "http://placehold.it/32x32",
-#     "age": 34,
-#     "name": "Kristie Cole",
-#     "gender": "female",
-#     "company": "QUADEEBO",
-#     "email": "kristiecole@quadeebo.com"
-#   },
-#   {
-#       "id":15,
-#     "balance": "$3,277.32",
-#     "picture": "http://placehold.it/32x32",
-#     "age": 30,
-#     "name": "Leonor Cross",
-#     "gender": "female",
-#     "company": "GRONK",
-#     "email": "leonorcross@gronk.com"
-#   },
-#   {
-#       "id":16,
-#     "balance": "$1,972.47",
-#     "picture": "http://placehold.it/32x32",
-#     "age": 28,
-#     "name": "Marsh Mccall",
-#     "gender": "male",
-#     "company": "ULTRIMAX",
-#     "email": "marshmccall@ultrimax.com"
-#   }
-# ]
---
+++ userPolicyBP.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request, render_template, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from usersBP import db
+
+
+User_policy_bp = Blueprint('User_policy_bp',__name__)
+
+
+class Policy(db.Model):
+    __tablename__ = "policies"
+    id = db.Column(db.String(50), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(100))
+    basePremium = db.Column(db.String(100))  # Assuming you meant "date of birth"
+    baseCoverage = db.Column(db.String(100))
+    pictureURL = db.Column(db.String(100))
+
+    # JSON - Keys
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "basePremium": self.basePremium,
+            "baseCoverage": self.baseCoverage,
+            "pictureURL": self.pictureURL
+        }
